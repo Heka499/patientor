d3c1a5abdb9aaa52ceb672f7486373ab9f2f3838
Heikki Järvinen <heikki.jarvinen1@edu.turkuamk.fi>
2024-08-14 15:42:19 +0300

    9.25 started

--- src/components/PatientPage/index.tsx
+++ src/components/PatientPage/index.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { Patient, Diagnosis } from "../../types";
+import { Patient, Diagnosis, Entry } from "../../types";
 import patientService from "../../services/patients";
 import diagnosesService from "../../services/diagnoses";
 
@@ -54,3 +54,66 @@ const PatientPage = () => {
 };
 
 export default PatientPage;
+
+const EntryDetails: React.FC<{ entry: Entry }> = ({ entry }) => {
+  switch (entry.type) {
+    case "Hospital":
+      return <HospitalEntry entry={entry} />;
+    case "OccupationalHealthcare":
+      return <OccupationalHealthcareEntry entry={entry} />;
+    case "HealthCheck":
+      return <HealthCheckEntry entry={entry} />;
+    default:
+      return assertNever(entry);
+  }
+};
+
+const HospitalEntry: React.FC<{ entry: HospitalEntry }> = ({ entry }) => {
+  return (
+    <div>
+      <h3>
+        {entry.date} <i className="hospital icon"></i>
+      </h3>
+      <p>{entry.description}</p>
+      <p>
+        Discharge: {entry.discharge.date} {entry.discharge.criteria}
+      </p>
+    </div>
+  );
+};
+
+const OccupationalHealthcareEntry: React.FC<{
+  entry: OccupationalHealthcareEntry;
+}> = ({ entry }) => {
+  return (
+    <div>
+      <h3>
+        {entry.date} <i className="stethoscope icon"></i> {entry.employerName}
+      </h3>
+      <p>{entry.description}</p>
+      {entry.sickLeave && (
+        <p>
+          Sick leave: {entry.sickLeave.startDate} - {entry.sickLeave.endDate}
+        </p>
+      )}
+    </div>
+  );
+};
+
+const HealthCheckEntry: React.FC<{ entry: HealthCheckEntry }> = ({ entry }) => {
+  return (
+    <div>
+      <h3>
+        {entry.date} <i className="user md icon"></i>
+      </h3>
+      <p>{entry.description}</p>
+      <p>Health check rating: {entry.healthCheckRating}</p>
+    </div>
+  );
+};
+
+const assertNever = (value: never): never => {
+  throw new Error(
+    `Unhandled discriminated union member: ${JSON.stringify(value)}`
+  );
+};
